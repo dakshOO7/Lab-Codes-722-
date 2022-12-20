67a3366555708034d0c2a8b6c1fd500dd57aeaa0
Daksh Mohan <121113388+dakshOO7@users.noreply.github.com>
2022-12-20 21:58:52 +0530

    Update Heap Sort(Experiment-20).py

--- Heap Sort(Experiment-20).py
+++ Heap Sort(Experiment-20).py
@@ -1,5 +1,5 @@
 def heapify(arr, n, i):
-    # Find largest among root and children
+    
     largest = i
     l = 2 * i + 1
     r = 2 * i + 2
@@ -10,7 +10,7 @@ def heapify(arr, n, i):
     if r < n and arr[largest] < arr[r]:
         largest = r
 
-    # If root is not largest, swap with largest and continue heapifying
+    
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
         heapify(arr, n, largest)
@@ -19,15 +19,15 @@ def heapify(arr, n, i):
 def heapSort(arr):
     n = len(arr)
 
-    # Build max heap
+   
     for i in range(n // 2, -1, -1):
         heapify(arr, n, i)
 
     for i in range(n - 1, 0, -1):
-        # Swap
+       
         arr[i], arr[0] = arr[0], arr[i]
 
-        # Heapify root element
+        
         heapify(arr, i, 0)
 
 
