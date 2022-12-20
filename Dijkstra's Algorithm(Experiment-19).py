2bd6f4b7112fca9cde10e60b2a9200b9d101a909
Daksh Mohan <121113388+dakshOO7@users.noreply.github.com>
2022-12-20 21:57:34 +0530

    Update Dijkstra's Algorithm(Experiment-19).py

--- Dijkstra's Algorithm(Experiment-19).py
+++ Dijkstra's Algorithm(Experiment-19).py
@@ -10,16 +10,13 @@ class Graph():
 		for node in range(self.V):
 			print(node, "\t\t", dist[node])
 
-	# A utility function to find the vertex with
-	# minimum distance value, from the set of vertices
-	# not yet included in shortest path tree
+	
 	def minDistance(self, dist, sptSet):
 
-		# Initialize minimum distance for next node
+		
 		min = 1e7
 
-		# Search not nearest vertex not in the
-		# shortest path tree
+		
 		for v in range(self.V):
 			if dist[v] < min and sptSet[v] == False:
 				min = dist[v]
@@ -27,9 +24,7 @@ class Graph():
 
 		return min_index
 
-	# Function that implements Dijkstra's single source
-	# shortest path algorithm for a graph represented
-	# using adjacency matrix representation
+	
 	def dijkstra(self, src):
 
 		dist = [1e7] * self.V
@@ -38,19 +33,13 @@ class Graph():
 
 		for cout in range(self.V):
 
-			# Pick the minimum distance vertex from
-			# the set of vertices not yet processed.
-			# u is always equal to src in first iteration
+			
 			u = self.minDistance(dist, sptSet)
 
-			# Put the minimum distance vertex in the
-			# shortest path tree
+			
 			sptSet[u] = True
 
-			# Update dist value of the adjacent vertices
-			# of the picked vertex only if the current
-			# distance is greater than new distance and
-			# the vertex in not in the shortest path tree
+			
 			for v in range(self.V):
 				if (self.graph[u][v] > 0 and
 				sptSet[v] == False and
